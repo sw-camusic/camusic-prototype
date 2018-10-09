b1af8027c3e32a2f7cf526a777c90b398342e99b
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-10-09 17:53:10 +0900

    add camera

--- src/script.js
+++ src/script.js
@@ -1,4 +1,16 @@
 window.addEventListener("DOMContentLoaded", () => {
+    navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+            facingMode: "environment",
+        },
+    }).then((stream) => {
+        let video = document.querySelector("video");
+        video.height = window.innerHeight;
+        video.srcObject = stream;
+        video.width = window.innerWidth;
+    });
+
     document.querySelectorAll(".music-play").forEach((value) => {
         value.addEventListener("click", () => {
             document.querySelector(`audio[src="${value.dataset.target}"]`).play();
